8ca4a9691b35e4bec51e6a87a64d6738b84c3d83
realespee <wanyamasp@gmail.com>
2021-03-17 12:35:21 +0300

    Updates

---
+++ precache-manifest.394ebead66cd943a6b9b293bceba4b1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1bbfa2850d0b27ff18f7f21596cfc64",
+    "url": "/consti-finder/index.html"
+  },
+  {
+    "revision": "dd13f43edf811747a13d",
+    "url": "/consti-finder/static/css/main.d9d4d2a8.chunk.css"
+  },
+  {
+    "revision": "00cee56e31c458f69810",
+    "url": "/consti-finder/static/js/2.7b8d3021.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/consti-finder/static/js/2.7b8d3021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd13f43edf811747a13d",
+    "url": "/consti-finder/static/js/main.3b052867.chunk.js"
+  },
+  {
+    "revision": "a9fdabc885a022feff34",
+    "url": "/consti-finder/static/js/runtime-main.14365808.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/consti-finder/precache-manifest.394ebead66cd943a6b9b293bceba4b1c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/consti-finder/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
